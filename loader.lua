0072bd095eaea30005133cd4da2efab59965ebea
irlydontknowastaken <jonathankim2008@gmail.com>
2021-06-10 18:06:32 -0400

    added a loader
    dont know if it works

--- loader.lua
+++ loader.lua
@@ -1,8 +1,8 @@
-if game.PlaceId == 0000000
-
+if game.PlaceId == 6749827639
+loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
 else if game.PlaceId == 0000000
 then print(game.PlaceId)
-else if game.PlaceId == 0000000
-then print(game.PlaceId)
+else if
+loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
 end
 end
