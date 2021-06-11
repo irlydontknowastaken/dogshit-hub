658ce68d08f765f1bff83c79e4865e0a6517a237
irlydontknowastaken <65200970+irlydontknowastaken@users.noreply.github.com>
2021-06-10 21:05:23 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,9 @@
-if game.PlaceId == 6749827639
+if game.PlaceId == 6749827639 then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
 else if game.PlaceId == 0000000
 then print(game.PlaceId)
-else if
+else if then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
+end
 end
 end
