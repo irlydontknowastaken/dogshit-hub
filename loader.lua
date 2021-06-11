b60d93bc3c5c5202881d45b972af4e2e38de1b17
irlydontknowastaken <jonathankim2008@gmail.com>
2021-06-11 08:50:34 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,16 @@
+testing = "no"
+
+if syn then
+if testing == "yes" then
+print("how")
+elseif then
+
+end
+
+
 local a = "abc"
 if game.PlaceId == 6749827639 then
-then print(game.PlaceId)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
 else if game.PlaceId == 0000000
 then print(game.PlaceId)
 else if a == "abc" then
