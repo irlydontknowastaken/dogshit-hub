b67aac953a28f3e805dd85f4245898eb0134eb42
irlydontknowastaken <65200970+irlydontknowastaken@users.noreply.github.com>
2021-06-10 21:07:36 -0400

    a dumbass var fixes everything
    i hate lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,9 @@
+local a = "abc"
 if game.PlaceId == 6749827639 then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
+then print(game.PlaceId)
 else if game.PlaceId == 0000000
 then print(game.PlaceId)
-else if then
+else if a == "abc" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/irlydontknowastaken/dogshit-hub/main/universal.lua", true))()
 end
 end
